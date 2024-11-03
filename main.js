92d4cc445feebcbb513fff0f582c6918a83a5154
adialm <142651819+adialm@users.noreply.github.com>
2024-11-03 08:25:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function getroute() {
+    var number = document.getElementById('numberInput').value;
+    fetch(`http://localhost:5000/calculate?number=${number}`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('result').innerHTML = `Factorial: ${data.result}`;
+            })
+        .catch(error => console.error('Error:', error));
+}
